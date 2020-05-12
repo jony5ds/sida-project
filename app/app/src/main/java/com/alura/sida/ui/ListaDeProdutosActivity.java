0e391361af6a6b9655d9db9f0cdec996d65ab6da
jony5ds <37637355+jony5ds@users.noreply.github.com>
2020-05-11 23:25:35 -0300

    Adicionando binding na tela de layout activity

--- app/app/src/main/java/com/alura/sida/ui/ListaDeProdutosActivity.java
+++ app/app/src/main/java/com/alura/sida/ui/ListaDeProdutosActivity.java
@@ -1,19 +1,20 @@
 package com.alura.sida.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
-
 import com.alura.sida.R;
+import com.alura.sida.databinding.ListaDeProdutosActivityBinding;
 
 public class ListaDeProdutosActivity extends AppCompatActivity {
+
+    ListaDeProdutosActivityBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lista_de_produtos_activity);
+        _binding = DataBindingUtil.setContentView(this,R.layout.lista_de_produtos_activity);
         getSupportActionBar().hide();
-
 
     }
 }
