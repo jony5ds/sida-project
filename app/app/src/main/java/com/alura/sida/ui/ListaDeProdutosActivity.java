449d81475eebef356ba42f2bdd5dd90413eb6f23
jony5ds <37637355+jony5ds@users.noreply.github.com>
2020-05-11 22:50:31 -0300

    Alterando o nome do layout da tela de produtos

--- app/app/src/main/java/com/alura/sida/ui/ListaDeProdutosActivity.java
+++ app/app/src/main/java/com/alura/sida/ui/ListaDeProdutosActivity.java
@@ -11,7 +11,7 @@ public class ListaDeProdutosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lista_de_produtos_activity);
         getSupportActionBar().hide();
 
 
