8babda281330a589c1339e771c82f3b79eabb843
jony5ds <37637355+jony5ds@users.noreply.github.com>
2020-05-14 20:16:58 -0300

    Adicionando construtores na classe Produto

--- app/app/src/main/java/com/alura/sida/model/ProdutoObj.java
+++ app/app/src/main/java/com/alura/sida/model/ProdutoObj.java
@@ -3,10 +3,25 @@ package com.alura.sida.model;
 import java.io.Serializable;
 
 public class ProdutoObj implements Serializable {
+
     private long produtoId;
     private String nome;
     private String marca;
     private float preco;
+    private float kg;
+
+    public ProdutoObj(String nome, String marca, float preco) {
+        this.nome   = nome;
+        this.marca  = marca;
+        this.preco  = preco;
+    }
+
+    public ProdutoObj(String nome, String marca, float preco,float kg) {
+        this.nome   = nome;
+        this.marca  = marca;
+        this.preco  = preco;
+        this.kg     = kg;
+    }
 
     public long getProdutoId() {
         return produtoId;
@@ -38,5 +53,13 @@ public class ProdutoObj implements Serializable {
 
     public void setPreco(float preco) {
         this.preco = preco;
+    }
+
+    public float getKg() {
+        return kg;
+    }
+
+    public void setKg(float kg) {
+        this.kg = kg;
     }
 }
